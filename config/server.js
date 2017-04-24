197dec448f451997385f3da4bd1fa51cf3fd238f
Caio De Albuquerque Lucena Medeiros <camedeiros1@stefanini.com>
2017-04-24 14:27:03 -0300

    Change server

--- config/server.js
+++ config/server.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
     }
     next();
 });
-consign()
+consign({ cwd: process.cwd() + "/app" })
     .include('app/routes')
     .then('config/dbConnection.js')
     .then('app/schemas')
