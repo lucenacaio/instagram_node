e0466446470c49b87da8759f492f0dec656af058
Mateus Rodrigues de Oliveira <mroliveira1@stefanini.com>
2017-06-08 15:04:10 -0300

    git init

--- app.js
+++
@@ -1,5 +0,0 @@
-var app = require('./config/server');
-var port = 8080;
-app.listen(port, function() {
-    console.log("Server run on port " + port);
-})
--- config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'secret': 'I6nStO4gr4mS0CI4LM0M3nt5',
-};
